a0856293196ef5f8bb05a0cfc5c99079d7c73cc8
bssthu <bssthu@users.noreply.github.com>
2017-08-14 04:36:16 +0800

    create client

---
+++ client/ac_client.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+# -*- coding:utf-8 -*-
+# File          : ac_client.py
+# Author        : bssthu
+# Project       : rpi-ac
+# 
+
+
+import time
+import json
+import os
+try:
+    from urllib2 import urlopen
+except ImportError:
+    from urllib.request import urlopen
+
+
+if __name__ == '__main__':
+    last_state = 'on'
+    # 载入配置文件
+    with open('conf.json', 'rt') as fp:
+        config = json.load(fp)
+    while True:
+
+        try:
+            state = urlopen(config['server_url']).read()
+            if state != last_state:
+                print('new state: %s' % str(state))
+                cmd = config['command_dict'].get(state, None)
+                if cmd is not None:
+                    os.system(cmd)
+        except Exception as e:
+            print(e)
+        time.sleep(config['interval'])
