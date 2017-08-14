d6bdddf65ed29de42a3964e91baf47079873a87b
bssthu <bssthu@users.noreply.github.com>
2017-08-15 02:15:54 +0800

    add timestamp

--- client/ac_client.py
+++ client/ac_client.py
@@ -16,18 +16,23 @@ except ImportError:
 
 
 if __name__ == '__main__':
-    last_state = 'on'
+    last_stamp = None
     # 载入配置文件
     with open('conf.json', 'rt') as fp:
         config = json.load(fp)
     while True:
 
         try:
-            state = urlopen(config['server_url']).read()
-            if state != last_state:
-                last_state = state
-                print('new state: %s' % str(state))
-                cmd = config['command_dict'].get(state, None)
+            state = urlopen(config['server_url']).read().decode('utf-8')
+            ac_state, stamp = state.split('_', 1)
+            stamp = float(stamp)
+
+            if last_stamp is None:
+                last_stamp = stamp
+            elif stamp > last_stamp:
+                last_stamp = stamp
+                print('new state: %s' % str(ac_state))
+                cmd = config['command_dict'].get(ac_state, None)
                 if cmd is not None:
                     os.system(cmd)
         except Exception as e:
--- server/ac_server.py
+++ server/ac_server.py
@@ -6,11 +6,12 @@
 # 
 
 
+import time
 import flask
 app = flask.Flask(__name__)
 
 
-state = 'on'
+state = 'on_0'
 
 
 @app.route('/ac', methods=['GET'])
@@ -24,8 +25,9 @@ def on_check_ac_state():
 def on_set_ac_state():
     """设置空调状态"""
     global state
-    state = flask.request.form['ac_state']
-    return state
+    ac_state = flask.request.form['ac_state']
+    state = '%s_%f' % (ac_state, time.time())
+    return ac_state
 
 
 if __name__ == '__main__':
